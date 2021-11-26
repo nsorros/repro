babb5752437d29625e07a2eb1b7b4c5c88a1a889
Nick Sorros <nsorros@gmail.com>
2021-11-26 15:18:36 +0000

    Add train

---
+++ repro/train.py
@@ -0,0 +1,33 @@
+import pickle
+import json
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import SGDClassifier
+from sklearn.pipeline import Pipeline
+import typer
+
+
+def load_data(data_path):
+    texts = []
+    labels = []
+    with open(data_path) as f:
+        for line in f:
+            item = json.loads(line)
+            texts.append(item["text"])
+            labels.append(item["label"])
+    return texts, labels
+
+def train(data_path, model_path):
+    X, y = load_data(data_path)
+
+    pipeline = Pipeline([
+        ("tfidf", TfidfVectorizer()),
+        ("svm", SGDClassifier())
+    ])
+    pipeline.fit(X, y)
+
+    with open(model_path, "wb") as f:
+        f.write(pickle.dumps(model_path))
+
+if __name__ == "__main__":
+    typer.run(train)
